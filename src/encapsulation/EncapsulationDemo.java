976c233c2921fa78f6858e4096b6c9c331dd5078
Kathija-Shameema <kathijashameema2211@gmail.com>
2025-08-08 08:42:29 +0530

    java day 2

---
+++ src/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package encapsulation;
+
+public class EncapsulationDemo {
+		public static void main(String[] args) {
+			
+			
+			//object creation 
+			OopsconceptDemo obj = new OopsconceptDemo();
+			obj.setSerialNum(101);
+			obj.setName("Pooja");
+			obj.setAge(20);
+			System.out.println(obj);
+			
+		}
+}
---
+++ src/inheritance/Singleinheritance.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Singleinheritance {
+public static void main(String[] args) {
+		
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+		System.out.println(student);
+	}
+}
